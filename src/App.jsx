a063d8524235df04cf4783ee32e59284c0c3a752
Naveen Kumawat <naveendimond77@gmail.com>
2025-06-26 00:25:31 +0530

    feat: initialize Khushi Electronics project with React and Vite
    - Added package.json with dependencies and scripts for development, build, linting, and preview.
    - Included Vite logo SVG in the public directory.
    - Created App component and main entry point for the application.
    - Developed RefurbishedShop component to display refurbished laptops with styling.
    - Implemented CSS for layout and design of the application.
    - Configured Vite for React support and set up basic project structure.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import RefurbishedShopPage from "./components/RefurbishedShop";
+
+function App() {
+  return (
+    <div>
+      <RefurbishedShopPage/>
+    </div>
+  );
+}
+
+export default App;
