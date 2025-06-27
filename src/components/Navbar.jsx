a422541c4ed3d3d60c7091cca366524c0b91f16f
Naveen Kumawat <naveendimond77@gmail.com>
2025-06-27 18:28:45 +0530

    style: update Navbar styles for improved layout and aesthetics

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,9 @@ import '../App.css'
 function Navbar() {
   return (
     <>
-    <div className="nav text-black flex justify-between items-center p-4">
-      <div className="shop-name">KhusiLaptop</div>
-      <ul className="nav-links text-blue-900 flex gap-4">
+    <div className="nav w-full bg-blue-200 text-black flex justify-between items-center p-4">
+      <div className="shop-name w-1/4 italic">KhusiLaptop</div>
+      <ul className="text-blue-900 w-3/4  flex  justify-center gap-4">
         <li >Home</li>
         <li>Shop</li>
         <li>Contact</li>
